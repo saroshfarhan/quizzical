3dd7cfa8cf5f73cd0bcaeb7a735e03e9fcdd3708
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-11-06 16:46:01 +0530

    Added some style changes.

--- src/Components/intro.jsx
+++ src/Components/intro.jsx
@@ -6,6 +6,7 @@ export default function Intro(props){
         <div className="intro">
             <div className='main-comp'>
                 <h1>Quizzical</h1>
+                <h3>Test your Anime knowledge</h3>
                 <button type='button' onClick={props.handleClick}>Start Quiz</button>
             </div>
         </div>
